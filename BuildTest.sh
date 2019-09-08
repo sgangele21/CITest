38d28536c40ffeebb7ae4360d5b855a56b0ae986
Sgangele21 <sahil.gangele@gmail.com>
2019-09-07 21:15:19 -0400

    Changed os version to 12.2

--- BuildTest.sh
+++ BuildTest.sh
@@ -1,1 +1,1 @@
-xcodebuild test -project CIXcodeTests.xcodeproj -scheme CIXcodeTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4'
+xcodebuild test -project CIXcodeTests.xcodeproj -scheme CIXcodeTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2'
